8e22ef6d835dc5ded9754e5dd64bbf7f54ca533d
David Jöch <david.joech@catalysts.cc>
2016-11-17 23:17:22 +0100

    moved bundle.js into lib and auto-clean it on build

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: glob.sync("./src/components/**/*.js"),
     output: {
         path: __dirname,
-        filename: "bundle.js"
+        filename: "lib/bundle.js"
     },
     module: {
         loaders: [
