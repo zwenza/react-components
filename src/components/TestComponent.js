d5385b2c592c0c7f47bde8714d63a8be77394821
David Jöch <david.joech@catalysts.cc>
2016-11-19 15:05:18 +0100

    integrated blueprintjs and removed webpack to only transpile with babel

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import { Button, Alert } from '@blueprintjs/core';
 
 export default class TestComponent extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            showAlert: false
+        };
+
+        this.handleOpen = this.handleOpen.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    handleOpen() {
+        this.setState({
+            showAlert: true
+        });
+    }
+
+    handleClose() {
+        this.setState({
+            showAlert: false
+        })
+    }
+
     render() {
         return (
-            <h1>Hello World!</h1>
+            <div>
+                <Button
+                    onClick={this.handleOpen}
+                    className="pt-small"
+                    text="open dialog"
+                />
+                <Alert
+                    isOpen={this.state.showAlert}
+                    confirmButtonText="close"
+                    onConfirm={this.handleClose}
+                >
+                    <p>
+                        a <b>testdialog</b> !
+                    </p>
+                </Alert>
+            </div>
         )
     }
 }
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import TestComponent from './components/TestComponent';
+
+export {
+    TestComponent
+}
