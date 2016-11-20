e81c071d26a8926aa78a81b4a9a308c631a20462
David Jöch <david.joech@catalysts.cc>
2016-11-20 21:02:37 +0100

    added more styling options

--- src/components/login/LoginComponent.js
+++ src/components/login/LoginComponent.js
@@ -5,8 +5,12 @@ import { InputGroup, Button } from '@blueprintjs/core';
  * LoginComponent
  * @description: handles username and password
  * @props:
- *  {value}     buttonText: text of the register button
- *  {function}  handleLogin: get's called when the user clicks the login-button
+ *  {function}      (mandatory) handleLogin:        get's called when the user clicks the login-button
+ *  {value:string}  (optional)  buttonText:         text of the register button
+ *  {value:string}  (optional)  textFieldClass:     class name of the text-input fields
+ *  {value:string}  (optional)  buttonClass:        class name of the login-button
+ *  {value:string}  (optional)  firstPlaceholder:   placeholder of the first input-field
+ *  {value:string}  (optional)  secondPlaceholder:  placeholder of the second input-field
  */
 export default class LoginComponent extends React.Component {
     constructor() {
@@ -23,26 +27,26 @@ export default class LoginComponent extends React.Component {
 
     render() {
         const { username, password } = this.state;
-        const { buttonText } = this.props;
+        const { buttonText, textFieldClass, buttonClass, firstPlaceholder, secondPlaceholder } = this.props;
 
         return (
             /* TODO add more styling */
             <div className=".login-form">
                 <h3>sign in</h3>
                 <InputGroup
-                    className=".pt-large"
+                    className={textFieldClass ? textFieldClass : ".pt-large"}
                     placeholder="email"
                     type="email"
                     onChange={this.handleUsernameChange}
                     value={username.value} />
                 <InputGroup
-                    className=".pt-large"
+                    className={textFieldClass ? textFieldClass : ".pt-large"}
                     placeholder="password"
                     type="password"
                     onChange={this.handlePasswordChange}
                     value={password.value} />
                 <Button
-                    className="pt-small pt-intent-primary"
+                    className={buttonClass ? buttonClass : "pt-small pt-intent-primary"}
                     text={buttonText ? buttonText : "login"}
                     onClick={this.handleSubmit}/>
             </div>
