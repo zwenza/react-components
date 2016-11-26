50e272a063d87dc7d1b625b3f7eda9b8bc6072bb
David Jöch <david.joech@catalysts.cc>
2016-11-26 22:29:38 +0100

    added more create-component tests and deleted extra crudl-component

--- src/components/crudl/create/Create.spec.js
+++ src/components/crudl/create/Create.spec.js
@@ -1,11 +1,66 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 import Create from './Create';
+import {Button} from "react-bootstrap";
 
 describe('<Create />', () => {
     it('renders without crashing', () => {
         const create = shallow(
-            <Create />
+            <Create handleCreate={() => {}} model={{}}/>
         );
     });
+
+    it('returns correct new object on create', () => {
+        const handleCreate = jest.fn();
+        const modelObject = {
+            username: '',
+            password: ''
+        };
+
+        const create = shallow(
+            <Create handleCreate={handleCreate} model={modelObject}/>
+        );
+
+        // mock user-input data
+        create.setState({
+            model: {
+                username: {value: 'test@test.at'},
+                password: {value: 'test'}
+            }
+        });
+
+        expect(handleCreate).not.toBeCalled();
+        create.find(Button).simulate("click");
+        expect(handleCreate).toBeCalledWith({
+            username: 'test@test.at',
+            password: 'test'
+        });
+    });
+
+    it('returns a boolean value if specified in model', () => {
+        const handleCreate = jest.fn();
+        const modelObject = {
+            receiveNewsletter: false,
+            checkedAGBs: false
+        };
+
+        const create = shallow(
+            <Create handleCreate={handleCreate} model={modelObject}/>
+        );
+
+        // mock user-input data
+        create.setState({
+            model: {
+                receiveNewsletter: { checked: false },
+                checkedAGBs: { checked: true }
+            }
+        });
+
+        expect(handleCreate).not.toBeCalled();
+        create.find(Button).simulate("click");
+        expect(handleCreate).toBeCalledWith({
+            receiveNewsletter: false,
+            checkedAGBs: true
+        });
+    })
 });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import Login from './components/login/Login';
-import Crudl from './components/crudl/Crudl';
 import Create from './components/crudl/create/Create';
 
 export {
     Login,
-    Crudl,
     Create
 }
